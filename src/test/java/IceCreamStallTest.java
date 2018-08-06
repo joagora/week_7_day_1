7d0a37b0e5c6d55f7e71cd602ba6b8447151f740
Joanna Gora <joanna.gora@protonmail.com>
2018-08-06 22:52:41 +0100

    tested defaultPrice and priceFor methods in IceCreamStall

--- src/test/java/IceCreamStallTest.java
+++ src/test/java/IceCreamStallTest.java
@@ -1,4 +1,5 @@
 import Stalls.IceCreamStall;
+import Visitors.Visitor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,9 +8,11 @@ import static junit.framework.TestCase.assertEquals;
 public class IceCreamStallTest {
 
     IceCreamStall iceCreamStall;
+    Visitor visitor;
     @Before
     public void before(){
         iceCreamStall = new IceCreamStall("Iceland", "Zenon Mazurek", 3);
+        visitor = new Visitor("Mike", 13, 145, 14.00);
     }
 
     @Test
@@ -30,5 +33,16 @@ public class IceCreamStallTest {
     @Test
     public void hasRating(){
         assertEquals(0, iceCreamStall.getRating());
+    }
+
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(2.80, iceCreamStall.defaultPrice());
+    }
+
+    @Test
+    public void hasPriceForVisitor(){
+        assertEquals(2.80, iceCreamStall.priceFor(visitor));
     }
 }
