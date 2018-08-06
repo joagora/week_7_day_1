186ec41101e7444a730dd4e119be47ffda6a4dc7
Joanna Gora <joanna.gora@protonmail.com>
2018-08-06 21:06:28 +0100

    created Stall class

---
+++ src/main/java/Stalls/Stall.java
@@ -0,0 +1,18 @@
+package Stalls;
+
+public abstract class Stall {
+
+    private String name;
+    private String ownerName;
+    private int parkingSpot;
+
+    public Stall(String name, String ownerName, int parkingSpot){
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
