2af66b697a3f08c079dceff8fc4ba4989766cfa5
Joanna Gora <joanna.gora@protonmail.com>
2018-08-06 22:50:12 +0100

    tested priceFror in CandyFlossStall class)

--- src/test/java/CandyFLossStallTest.java
+++ src/test/java/CandyFLossStallTest.java
@@ -1,4 +1,5 @@
 import Stalls.CandyFlossStall;
+import Visitors.Visitor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,9 +8,11 @@ import static junit.framework.TestCase.assertEquals;
 public class CandyFLossStallTest {
 
     CandyFlossStall candyFlossStall;
+    Visitor visitor;
     @Before
     public void before(){
         candyFlossStall = new CandyFlossStall("Candy Crash", "Lolla Pop", 2);
+        visitor = new Visitor("Raphael", 15, 156, 12.00);
     }
 
     @Test
@@ -30,5 +33,10 @@ public class CandyFLossStallTest {
     @Test
     public void hasDefaultPrice(){
         assertEquals(4.20, candyFlossStall.defaultPrice());
+    }
+
+    @Test
+    public void hasPriceForVisitor(){
+        assertEquals(4.20, candyFlossStall.priceFor(visitor));
     }
 }
