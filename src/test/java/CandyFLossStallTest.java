4c231ac9f76f36cdddf8c3b71542ab7fccdd48b5
Joanna Gora <joanna.gora@protonmail.com>
2018-08-06 21:27:29 +0100

    created and tested getter for name in CandyFlossStall

---
+++ src/test/java/CandyFLossStallTest.java
@@ -0,0 +1,21 @@
+import Stalls.CandyFlossStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CandyFLossStallTest {
+
+    CandyFlossStall candyFlossStall;
+    @Before
+    public void before(){
+        candyFlossStall = new CandyFlossStall("Candy Crash", "Lolla Pop", 2);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Candy Crash", candyFlossStall.getName());
+    }
+
+
+}
