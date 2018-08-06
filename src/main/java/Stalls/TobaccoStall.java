d4c94330a22463411cc931af9d7781f801a8070a
Joanna Gora <joanna.gora@protonmail.com>
2018-08-06 23:11:25 +0100

    tested isAllowed method for TobaccoStall

--- src/main/java/Stalls/TobaccoStall.java
+++ src/main/java/Stalls/TobaccoStall.java
@@ -1,8 +1,9 @@
 package Stalls;
 
+import Interfaces.ISecurity;
 import Visitors.Visitor;
 
-public class TobaccoStall extends Stall {
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, int parkingSpot){
         super(name, ownerName, parkingSpot);
@@ -16,5 +17,14 @@ public class TobaccoStall extends Stall {
     @Override
     public double priceFor(Visitor visitor){
         return defaultPrice();
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor){
+        if (visitor.getAge() >= 18){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/test/java/TobaccoStallTest.java
+++ src/test/java/TobaccoStallTest.java
@@ -9,10 +9,16 @@ public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
     Visitor visitor;
+    Visitor visitorJoanna;
+    Visitor visitorRaphael;
+    Visitor visitorMike;
     @Before
     public void before(){
         tobaccoStall = new TobaccoStall("Pipes", "John Smoke", 5);
-        visitor = new Visitor("Mike", 12, 134, 12.00);
+        visitorMike = new Visitor("Mike", 12, 134, 12.00);
+        visitorRaphael = new Visitor("Raphael", 18, 120, 12.00);
+        visitorJoanna = new Visitor("Joanna", 25, 170, 16.00);
+
     }
 
     @Test
@@ -37,6 +43,21 @@ public class TobaccoStallTest {
 
     @Test
     public void hasPriceForVisitor(){
-        assertEquals(6.60, tobaccoStall.priceFor(visitor));
+        assertEquals(6.60, tobaccoStall.priceFor(visitorMike));
+    }
+
+    @Test
+    public void canCheckIfAllowedOner18(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitorJoanna));
+    }
+
+    @Test
+    public void canCheckIfAllowedAge18(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitorRaphael));
+    }
+
+    @Test
+    public void canCheckIfAllowedUnderAge18(){
+        assertEquals(false, tobaccoStall.isAllowedTo(visitorMike));
     }
 }
